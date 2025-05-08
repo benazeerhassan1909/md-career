8d067b76b7159423c658e99191cb50473d38b970
benazeerhassan1909 <91200605+benazeerhassan1909@users.noreply.github.com>
2025-05-08 11:06:00 +0530

    Stop tracking .next directory

--- .next/server/app/api/draft-mode/enable/route.js
+++
@@ -1,8 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/draft-mode/enable/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_@sanity_57c21143._.js");
-runtime.loadChunk("server/chunks/node_modules_f11b156f._.js");
-runtime.loadChunk("server/chunks/[root of the server]__6f00f416._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/draft-mode/enable/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/draft-mode/enable/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/draft-mode/enable/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/app/sitemap.xml/route.js
+++
@@ -1,8 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/sitemap.xml/route.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_@sanity_491a448e._.js");
-runtime.loadChunk("server/chunks/node_modules_b913f7ae._.js");
-runtime.loadChunk("server/chunks/[root of the server]__6712ee30._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/sitemap.xml/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/sitemap--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/sitemap--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
